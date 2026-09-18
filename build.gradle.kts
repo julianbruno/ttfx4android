441860cf46c6b933c64d7c533e1177c4a4b588a4
Julian Bruno <julianbruno@gmail.com>
2026-09-18 20:58:48 -0300

    build: upgrade Kotlin and Compose compiler plugin to 2.1.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    kotlin("jvm") version "2.1.0" apply false
+    kotlin("jvm") version "2.1.10" apply false
     id("com.android.library") version "8.8.0" apply false
     id("com.android.application") version "8.8.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.10" apply false
 }
 
 tasks.register("clean", Delete::class) {
