550d968a38b29cdda089ecc30031e73a3e90d6ab
Julian Bruno <julianbruno@gmail.com>
2026-09-18 23:48:37 -0300

    build(gradle): update to gradle 9.6 and agp 9.4.1 sync configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     kotlin("jvm") version "2.1.10" apply false
-    id("com.android.library") version "8.13.2" apply false
-    id("com.android.application") version "8.13.2" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.10" apply false
+    id("com.android.library") version "9.4.1" apply false
+    id("com.android.application") version "9.4.1" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
 }
 
 allprojects {
